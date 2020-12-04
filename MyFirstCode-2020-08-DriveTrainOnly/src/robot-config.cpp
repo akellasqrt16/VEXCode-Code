63d3b3fe605a43ecf406ebc0ba4e4bf527f4649c
akellasqrt16 <64755067+akellasqrt16@users.noreply.github.com>
2020-12-04 09:46:32 -0800

    Update robot-config.cpp

--- MyFirstCode-2020-08-DriveTrainOnly/src/robot-config.cpp
+++ MyFirstCode-2020-08-DriveTrainOnly/src/robot-config.cpp
@@ -3,7 +3,7 @@
 using namespace vex;
 
 // A global instance of brain used for printing to the V5 brain screen
-brain Brain;
+
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
