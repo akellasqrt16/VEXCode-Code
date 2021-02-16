371aaf5e492ce1af1955031d7651a9e6f2628321
ramyak4 <76259881+ramyak4@users.noreply.github.com>
2021-02-15 18:27:00 -0800

    auton

--- MyFirstCode-2020-08-DriveTrainOnly/src/main.cpp
+++ MyFirstCode-2020-08-DriveTrainOnly/src/main.cpp
@@ -1,9 +1,8 @@
-// DRIVER CODE POG
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       pranavakella                                              */
-/*    Created:      Mon Aug 10 2020                                           */
+/*    Author:       Ramya                                                     */
+/*    Created:      Mon Feb 15 2021                                           */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
@@ -11,31 +10,14 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
-
-//Before running this code, make sure the ports for the motors are identical to the ports mentioned in the code
-//To move the left motor, move the left stick, while to move the right motor, move the right stick
-
-
-
-
-
-
-
-
 #include "vex.h"
 
 using namespace vex;
-// idk why i'm including this stuff but the tutorial I watched had this
-vex::brain    Brain;
-vex::competition    Competition;
 
-//Setting up the motors we are going to use
-
-//Make sure to CHANGE THE PORT NUMBER TO THE PORT THAT THE CORRESPONDING MOTOR IS PLUGGED INTO
 
 //Drivetrain
 vex::motor  leftTopDriveMotor = vex::motor( vex:: PORT3);
-vex::motor  rightTopDriveMotor = vex::motor( vex:: PORT18,true);
+vex::motor  rightTopDriveMotor = vex::motor( vex:: PORT1,true);
 vex::motor  leftBottomDriveMotor = vex::motor( vex:: PORT19,true);
 vex::motor  rightBottomDriveMotor = vex::motor( vex:: PORT20);
 
@@ -56,168 +38,39 @@ vex::motor  RightIntake = vex::motor( vex:: PORT10, true); //Change port
 
 vex::controller Yeetroller = vex::controller();
 
-//Stuff like calibrating sensors, positioning lift, etc.
-void setup (void) {
 
+void moveForward(int rotations){
+  int degrees = rotations*360;
+  leftTopDriveMotor.startRotateFor(vex::directionType::fwd, degrees, vex::rotationUnits::deg);
+  rightTopDriveMotor.startRotateFor(vex::directionType::fwd, degrees, vex::rotationUnits::deg);
+  leftBottomDriveMotor.startRotateFor(vex::directionType::fwd, degrees, vex::rotationUnits::deg);
+  rightBottomDriveMotor.rotateFor(vex::directionType::fwd, degrees, vex::rotationUnits::deg);
+}
+
+void turnRight(){
+  int degrees = 0;
+  leftTopDriveMotor.startRotateFor(vex::directionType::fwd, degrees, vex::rotationUnits::deg);
+  rightTopDriveMotor.startRotateFor(vex::directionType::rev, degrees, vex::rotationUnits::deg);
+  leftBottomDriveMotor.startRotateFor(vex::directionType::fwd, degrees, vex::rotationUnits::deg);
+  rightBottomDriveMotor.rotateFor(vex::directionType::rev, degrees, vex::rotationUnits::deg);
+
+}
+
+void turnLeft(){
+  int degrees = 0;
+  leftTopDriveMotor.startRotateFor(vex::directionType::rev, degrees, vex::rotationUnits::deg);
+  rightTopDriveMotor.startRotateFor(vex::directionType::fwd, degrees, vex::rotationUnits::deg);
+  leftBottomDriveMotor.startRotateFor(vex::directionType::rev, degrees, vex::rotationUnits::deg);
+  rightBottomDriveMotor.rotateFor(vex::directionType::fwd, degrees, vex::rotationUnits::deg);
 
 }
 
 
 
-int runoff(int parameter) {
-  if (parameter > 100){
-    parameter = 100;
-  }
-  else if (parameter < -100){
-    parameter = -100;
-  }
-  return parameter;
-}
-
-
-
-
-
-int overFlow(int input){
-  if (input < 30 && input > 0){
-    input = 0;
-  } else if (input > -30 && input < 0){
-    input = 0;
-  }
-  return input;
-}
-
-
-
-
-int average(int input1, int input2){
-  return (input1+input2)/2;
-}
-
-
-
-//User control period (and general)
-int main() {
+void auton() {
   // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-  
-  //variables
-
-  //Percent speed that the each part moves at
-  int vcbSpeed = 100;
-
-  int intakeSpeed = 100;
 
   
-  
-  // infinite loop for the controller binds
-  while (1) {
 
 
-
-//Drive Train Program
-  //Left Stick  
-    int leftForward = Yeetroller.Axis3.position();
-    int leftSideways = Yeetroller.Axis4.position();
-    int rightSideways = Yeetroller.Axis1.position();  
-
-
-
-    int LT = leftForward + leftSideways + rightSideways;
-    LT = runoff(LT);
-    LT = overFlow(LT);
-    int RT = leftForward - leftSideways - rightSideways;
-    RT = runoff(RT);
-    RT = overFlow(RT);
-    int LB = leftForward - leftSideways + rightSideways;
-    LB = runoff(LB);
-    LB = overFlow(LB);
-    int RB = leftForward + leftSideways - rightSideways;
-    RB = runoff(RB);
-    RB = overFlow(RB);
-    
-
-
-
-
-
-
-    
-    leftTopDriveMotor.spin(vex::directionType::fwd, LT, vex::velocityUnits::pct);
-    rightBottomDriveMotor.spin(vex::directionType::rev, RB, vex::velocityUnits::pct);
-    leftBottomDriveMotor.spin(vex::directionType::rev, LB, vex::velocityUnits::pct);
-    rightTopDriveMotor.spin(vex::directionType::fwd, RT, vex::velocityUnits::pct);
-
-  
-    
-    
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-
-    //Intakes Program
-
-
-    //If Button 1 is pressed, the Intakes will move inwards
-    if (Yeetroller.ButtonR1.pressing()) {
-        LeftIntake.spin(vex::directionType::fwd, intakeSpeed, vex::velocityUnits::pct);
-        RightIntake.spin(vex::directionType::fwd, intakeSpeed, vex::velocityUnits::pct);
-    }
-    //If Button 2 is pressed, the intakes will move outwards
-    else if (Yeetroller.ButtonR2.pressing()){
-      LeftIntake.spin(vex::directionType::rev, intakeSpeed, vex::velocityUnits::pct);
-      RightIntake.spin(vex::directionType::rev, intakeSpeed, vex::velocityUnits::pct);
-    }
-    //If nothing is pressed, the intakes will stay stationary
-    else {
-      LeftIntake.stop(vex::brakeType::brake);
-      RightIntake.stop(vex::brakeType::brake);
-    }
-
-
-    //VCB Program:
-
-    //If Button 3 is pressed, the vcb will move forward
-    if (Yeetroller.ButtonL1.pressing()) {
-        vcb.spin(vex::directionType::rev, vcbSpeed, vex::velocityUnits::pct);
-    }
-    //If Button 3 is pressed, the vcb will move in the opposite direction
-    else if (Yeetroller.ButtonL2.pressing()) {
-        vcb.spin(vex::directionType::fwd, vcbSpeed, vex::velocityUnits::pct);
-    }
-    //If nothing is pressed, the vcb will stay stationary
-    else {
-      vcb.stop(vex::brakeType::brake);
-    }
-
-    //shell Program:
-
-    //If Button 3 is pressed, the vcb will move forward
-    if (Yeetroller.ButtonA.pressing()) {
-        shell.spin(vex::directionType::rev, vcbSpeed, vex::velocityUnits::pct);
-    }
-    //If Button 3 is pressed, the vcb will move in the opposite direction
-    else if (Yeetroller.ButtonX.pressing()) {
-        shell.spin(vex::directionType::fwd, vcbSpeed, vex::velocityUnits::pct);
-    }
-    //If nothing is pressed, the vcb will stay stationary
-    else {
-      shell.stop(vex::brakeType::brake);
-    }
-
-
-    vex::task::sleep(20); //Apparently prevents wasted resources yeah idk the tutorial had it so I kept it
-
-  }
-  
 }
